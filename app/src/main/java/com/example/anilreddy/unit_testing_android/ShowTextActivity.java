a32bffeffd108743514d8a0386f5ceb5ec2916c1
AnilRdyAleti <AnilRdyAleti@github.com>
2018-07-11 09:56:12 -0400

    Added unit testing libraries in gradle file and completed show text activity implementation

--- app/src/main/java/com/example/anilreddy/unit_testing_android/ShowTextActivity.java
+++ app/src/main/java/com/example/anilreddy/unit_testing_android/ShowTextActivity.java
@@ -1,13 +1,31 @@
 package com.example.anilreddy.unit_testing_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.common.base.Strings;
 
 public class ShowTextActivity extends AppCompatActivity {
+
+    public final static String KEY_EXTRA_MESSAGE = "com.example.anilreddy.unit_testing_android.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_text);
+
+        Intent intent = getIntent();
+        String message = Strings.nullToEmpty(intent.getStringExtra(KEY_EXTRA_MESSAGE));
+
+        ((TextView) findViewById(R.id.show_text_view)).setText(message);
+    }
+
+    static protected Intent newStartIntent(Context context, String message) {
+        Intent newIntent = new Intent(context, ShowTextActivity.class);
+        newIntent.putExtra(KEY_EXTRA_MESSAGE, message);
+        return newIntent;
     }
 }
