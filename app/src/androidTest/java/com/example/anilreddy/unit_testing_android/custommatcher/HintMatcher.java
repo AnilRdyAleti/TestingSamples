45291e7e0d8245ac582dcf2c12fd80264a5cc48e
AnilRdyAleti <AnilRdyAleti@github.com>
2018-07-11 13:52:50 -0400

    checked all the case to for matcher

--- app/src/androidTest/java/com/example/anilreddy/unit_testing_android/custommatcher/HintMatcher.java
+++ app/src/androidTest/java/com/example/anilreddy/unit_testing_android/custommatcher/HintMatcher.java
@@ -1,4 +1,35 @@
 package com.example.anilreddy.unit_testing_android.custommatcher;
 
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.hamcrest.Matchers.is;
+
 public class HintMatcher {
+
+    static Matcher<View> withHint(final String subString) {
+        return withHint(is(subString));
+    }
+
+    static Matcher<View> withHint(final Matcher<String> stringMatcher) {
+        checkNotNull(stringMatcher);
+        return new BoundedMatcher<View, EditText>(EditText.class) {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with hint: ");
+                stringMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(EditText view) {
+                final CharSequence hint = view.getHint();
+                return hint != null && stringMatcher.matches(hint.toString());
+            }
+        };
+    }
 }
