333ced04ebf16716a2665cecc507e83a501f09f5
AnilRdyAleti <AnilRdyAleti@github.com>
2018-07-17 14:09:48 -0400

    completed dialer activity test and tested valideding phone number

--- app/src/androidTest/java/com/example/anilreddy/unit_testing_android/intentsample/DialerActivityTest.java
+++ app/src/androidTest/java/com/example/anilreddy/unit_testing_android/intentsample/DialerActivityTest.java
@@ -1,4 +1,94 @@
 package com.example.anilreddy.unit_testing_android.intentsample;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.anilreddy.unit_testing_android.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.app.Instrumentation.ActivityResult;
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class DialerActivityTest {
+
+    private static final String VALID_PHONE_NUMBER = "123-345-6789";
+
+    private static final Uri INTENT_DATA_PHONE_NUMBER = Uri.parse("tel: " + VALID_PHONE_NUMBER);
+
+    private static String PACKAGE_ANDROID_DIALER = "com.android.phone";
+
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            PACKAGE_ANDROID_DIALER = "com.android.server.telecom";
+        }
+    }
+
+    @Rule
+    public IntentsTestRule<DialerActivity> mActivityRule = new IntentsTestRule<>(DialerActivity.class);
+
+    @Before
+    public void stubAllExternalIntents() {
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
+    @Before
+    public void grantPhonePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getInstrumentation().getUiAutomation().executeShellCommand("pm grant " + getTargetContext()
+                    .getPackageName() + "android.permission.CALL_PHONE");
+        }
+    }
+
+    @Test
+    public void typeNumber_ValidInput_InitiatesCall() {
+
+        onView(withId(R.id.edit_text_caller_number)).perform(typeText(VALID_PHONE_NUMBER), closeSoftKeyboard());
+        onView(withId(R.id.button_call_number)).perform(click());
+
+        intended(allOf(hasAction(Intent.ACTION_CALL),
+                hasData(INTENT_DATA_PHONE_NUMBER),
+                toPackage(PACKAGE_ANDROID_DIALER)));
+    }
+
+    @Test
+    public void pickContactButton_click_SelectsPhoneNumber() {
+        intending(hasComponent(hasShortClassName(".ContactsActivity")))
+                .respondWith(new ActivityResult(Activity.RESULT_OK,
+                        ContactsActivity.createResultData(VALID_PHONE_NUMBER)));
+
+        onView(withId(R.id.button_pick_contact)).perform(click());
+
+        onView(withId(R.id.edit_text_caller_number)).check(matches(withText(VALID_PHONE_NUMBER)));
+    }
+
 }
